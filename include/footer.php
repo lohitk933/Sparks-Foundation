8bbb70131c30e57db83332e61bf5d0ce36cac52d
Lohit Kumar A Bhattangi <107184814+lohitk933@users.noreply.github.com>
2023-03-17 19:27:09 +0530

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,11 @@
+      <!-- footer part start -->
+      <footer class="py-3 my-4">
+        <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+          <li class="nav-item"><a href="index.php" class="nav-link px-2 text-muted">Home</a></li>
+          <li class="nav-item"><a href="customer.php" class="nav-link px-2 text-muted">View all Customers</a></li>
+          <li class="nav-item"><a href="about.php" class="nav-link px-2 text-muted">About</a></li>
+          <li class="nav-item"><a href="our_services.php" class="nav-link px-2 text-muted">Services</a></li>
+        </ul>
+        <p class="text-center text-muted">Lohit K A B © 2023 Banking System</p>
+      </footer>
+      <!-- footer part end -->
